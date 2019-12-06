dafa443fb87681d9d5367860a61697bae1151e7e
Tim <ptcheng@ucsd.ede>
2019-12-06 15:16:09 -0800

    cant go back to list after log out

--- public/jsmodules/logout.mjs
+++ public/jsmodules/logout.mjs
@@ -1,18 +1,17 @@
 import { sendRequest, base_url } from "./util.mjs";
 
 export function handleLogout(){
-    var key = localStorage.getItem("XmasWishlist_access_key")
+    var key = localStorage.getItem("XmasWishlist_key");
     sendRequest("POST", base_url + "/Users/logout" + "?access_token=" + key, resLogout, null);
 }
 
 function resLogout(xhr){
     if (xhr.readyState == 4 && xhr.status == 204) {
         alert("Logout Successful, returning to login page");
-        localStorage.removeItem("XmasWishlist_access_key");
-        window.location.href = "login.html";
     }else if(xhr.readyState == 4 && xhr.status == 401) {
         alert("Logout failed, you should have been logged out or access key timed out");
-        localStorage.removeItem("XmasWishlist_access_key");
-        window.location.href = "login.html";
     }
+    localStorage.removeItem("XmasWishlist_user");
+    localStorage.removeItem("XmasWishlist_key");
+    window.location.href = "login.html";
 }
