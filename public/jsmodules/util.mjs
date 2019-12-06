fcc34c4b71b075c044d282e631357ea9f5a2cc6d
Tim <ptcheng@ucsd.ede>
2019-12-06 10:57:36 -0800

    added some xhr functions

--- public/jsmodules/util.mjs
+++ public/jsmodules/util.mjs
@@ -1,6 +1,34 @@
-export function createElementToParent(parent, child_type, inner_html){
+export function createElementToParent(parent, child_type, inner_html) {
     var child = document.createElement(child_type);
     child.innerHTML = inner_html;
     parent.appendChild(child);
     return child;
+}
+
+export function createXHR() {
+    try { return new XMLHttpRequest(); } catch (e) { }
+    try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); } catch (e) { }
+    try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); } catch (e) { }
+    try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) { }
+    try { return new ActiveXObject("Microsoft.XMLHTTP"); } catch (e) { }
+    alert("XMLHttpRequest not supported");
+    return null;
+}
+
+export function sendRequest(method, url, readyState, status, reactionFunction)
+{
+    var xhr = createXHR();
+ 
+    if (xhr)
+     {
+       xhr.open(method,url,true);
+      xhr.onreadystatechange = function(){handleResponse(xhr, readyState, status, reactionFunction);};
+      xhr.send(null);
+     }
+}
+
+function handleResponse(xhr, readyState, status, reactionFunction){
+    if ( xhr.readyState == readyState && xhr.status == status){
+        reactionFunction();
+    }
 }
